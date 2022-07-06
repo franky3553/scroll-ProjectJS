ccbb187c228c7f792f962ffd8a9dc4d53d60f4e2
franky3553 <franky3553@gmail.com>
2022-07-06 22:29:50 +0200

    Add navToggle dynamically

--- app.js
+++ app.js
@@ -7,8 +7,21 @@
 const date = document.querySelector('.date');
 date.innerHTML = new Date().getFullYear();
 
-
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function(){
+   const containerHeight = linksContainer.getBoundingClientRect().height;
+   const linksHeight = links.getBoundingClientRect().height;
+   console.log(linksHeight)
+   if (containerHeight === 0){
+      linksContainer.style.height = `${linksHeight}px`
+   } else {
+      linksContainer.style.height = 0;
+   }
+});
 
 // ********** fixed navbar ************
 
